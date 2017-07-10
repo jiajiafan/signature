32c4fc0bd049f553212325ae71e85b364d5be233
jiajiafan <623254517@qq.com>
2017-07-10 19:12:46 +0800

    在线签名框

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ajia on 2017/6/21.
+ */
+var http=require("http"),
+    url=require("url"),
+    fs=require('fs'),
+    mime = require("mime");
+var sv=http.createServer(function(req,res){
+    if(req.url=="/saveData"){
+        req.on("data",function(data){
+            console.log("接收到的数据："+data);
+            fs.appendFile("dataJson.json",data,'utf-8',function(error,data){
+            });
+        });
+        req.on("end",function(){
+            console.log("完了")
+        });
+        res.end();
+    }
+    fs.readFile("."+req.url,function(err,data){
+        res.writeHead(200, {'content-type': mime.lookup(req.url) + ";charset=utf-8;"});
+        res.end(data);
+    });
+});
+sv.listen("666",function(){
+    console.log("server is create success,listen on 666 port!")
+});
